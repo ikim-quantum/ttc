1fa255e105dda04b7b4b6e266b28a95f10607e9d
Isaac Kim <ikim@Isaacs-MBP.modem>
2021-03-18 09:29:26 +1100

    Added toric.hpp. Functions for stabilizers.

---
+++ include/toric.hpp
@@ -0,0 +1,9 @@
+#ifndef TORIC_HPP_
+#define TORIC_HPP_
+
+std::vector<int> stab_x_bulk(int d);
+std::vector<int> stab_z_bulk(int d);
+std::vector<int> stab_x_bdy(int d);
+std::vector<int> stab_z_bdy(int d);
+
+#endif // TORIC_HPP_
