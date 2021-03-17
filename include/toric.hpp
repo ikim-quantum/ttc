70cf40c6e8d7477fce8d421c72bd7d2aa3cfc8c0
Isaac Kim <ikim@Isaacs-MBP.modem>
2021-03-18 10:02:05 +1100

    added bulk stabilizer functions

--- include/toric.hpp
+++ include/toric.hpp
@@ -1,6 +1,10 @@
 #ifndef TORIC_HPP_
 #define TORIC_HPP_
 
+int coordinate2num(int x, int y, int d);
+int num2x(int n, int d);
+int num2y(int n, int d);
+
 std::vector<int> stab_x_bulk(int d);
 std::vector<int> stab_z_bulk(int d);
 std::vector<int> stab_x_bdy(int d);
