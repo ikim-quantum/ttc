4f6d6525171243a3ce3e40bf06ed0aec773e60a1
Isaac Kim <ikim@vlan-2693-10-17-124-65.staff.wireless.sydney.edu.au>
2021-04-16 10:54:08 +1000

    Added non-uniform code distance functionality.

--- include/toric.hpp
+++ include/toric.hpp
@@ -11,4 +11,14 @@ std::vector<int> stab_z_bulk(int d);
 std::vector<int> stab_x_bdy(int d);
 std::vector<int> stab_z_bdy(int d);
 
+// "nu" means non-uniform, i.e., (possibly) different values of x- and z-distance. 
+int coordinate2num_nu(int x, int y, int dx, int dz);
+int num2x_nu(int n, int dx, int dz);
+int num2y_nu(int n, int dx, int dz);
+
+std::vector<int> stab_x_bulk_nu(int dx, int dz);
+std::vector<int> stab_z_bulk_nu(int dx, int dz);
+std::vector<int> stab_x_bdy_nu(int dx, int dz);
+std::vector<int> stab_z_bdy_nu(int dx, int dz);
+
 #endif // TORIC_HPP_
