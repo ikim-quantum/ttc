d53a3fe71c82f41e6cb147a66ff4a45848a97c61
Isaac Kim <ikim@Isaacs-MBP.modem>
2021-04-26 11:49:06 +1000

    Added stabilizer class

---
+++ include/stabilizer.hpp
@@ -0,0 +1,12 @@
+#ifndef STABILIZER_HPP_
+#define STABILIZER_HPP_
+
+class Stabilizer{
+public:
+  int x;
+  int y;
+  int bitrep;
+  Stabilizer(int xx, int yy, int bb);
+}
+
+#endif // STABILIZER_HPP_
