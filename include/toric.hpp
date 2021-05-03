17c8d731cc2f7c6ee0a547cbf81bc776d7491ddd
Isaac Kim <ikim@vlan-2693-10-17-124-65.staff.wireless.sydney.edu.au>
2021-05-03 11:27:14 +1000

    Added comments.

--- include/toric.hpp
+++ include/toric.hpp
@@ -3,28 +3,33 @@
 #include <vector>
 #include "stabilizer.hpp"
 
+// Print a bitstring, arranged on a dx x dz lattice.
 void print_surface(int dx, int dz, int bitstring);
 
-
-// "nu" means non-uniform, i.e., (possibly) different values of x- and z-distance. 
+// Helper functions for converting between abstract index and 2D coordinates.
 int coordinate2num(int x, int y, int dx, int dz);
 int num2x(int n, int dx, int dz);
 int num2y(int n, int dx, int dz);
 
+// Stabilizers (Bulk/Boundary and X/Z)
 Stabilizers stab_x_bulk(int dx, int dz);
 Stabilizers stab_z_bulk(int dx, int dz);
 Stabilizers stab_x_bdy(int dx, int dz);
 Stabilizers stab_z_bdy(int dx, int dz);
 
+// Stabilizers (X/Z)
 Stabilizers xchecks(int dx, int dz);
 Stabilizers zchecks(int dx, int dz);
 
+// Logical operators (Only one representative)
 int logical_x(int dx, int dz);
 int logical_z(int dx, int dz);
 
+// Measure stabilizers
 void measure_stab_x(Stabilizers SX, int xstring);
 void measure_stab_z(Stabilizers SZ, int zstring);
 
+// Distance between stabilizers on a 2D lattice
 int distance(Stabilizer s1, Stabilizer s2);
 
 #endif // TORIC_HPP_
